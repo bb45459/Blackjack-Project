beee3cd6872ca8c8a8c9c7464782a8d549b9471a
bb45459 <wbloebau@nd.edu>
2014-04-15 11:44:03 -0400

    Test Comment

--- split.m
+++ split.m
@@ -12,3 +12,5 @@ newplayers{player}{2,1} = newplayers{player}{1,2};
 newplayers{player}{1,2} = [];
 
 newdeck(length(newplayers)+1+player).location=['Player ' num2str(player) ' hand 2'];
+
+% hello GitHub
