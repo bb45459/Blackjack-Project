2e996cd88ed58ba158a349899b9ec8bb1a5de4af
bb45459 <wbloebau@nd.edu>
2014-04-15 11:33:55 -0400

    split.m

---
+++ split.m
@@ -0,0 +1,14 @@
+function [newplayers, newdeck]=split(players,deck,player)
+%split.m
+
+% function for splitting cards
+newdeck=deck;
+newplayers=players;
+
+newplayers{player}{2,1}= [];
+
+
+newplayers{player}{2,1} = newplayers{player}{1,2};
+newplayers{player}{1,2} = [];
+
+newdeck(length(newplayers)+1+player).location=['Player ' num2str(player) ' hand 2'];
